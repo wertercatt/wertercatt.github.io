1cfaf0690649b77deffcc98c0e13126829f0cd46
Blaze Marshall <wertercatt@wertercatt.com>
2022-04-05 17:05:46 -0500

    Initial WIP demo

--- SolanaNFTTracker/fractalScrape.py
+++ SolanaNFTTracker/fractalScrape.py
@@ -5,7 +5,7 @@ import json
 #Fractal.is internal API
 fractalAdminProjectManage = "https://fractal-core-grpc-server-rest-2-tmg6xh47ja-uc.a.run.app/admin/v1/project/manage/5679095853613056/collection/manage"
 fractalAdminProjectHandle = "https://fractal-core-grpc-server-rest-2-tmg6xh47ja-uc.a.run.app/admin/v1/project/handle/cinder"
-fractalAdminHistory = "https://fractal-core-grpc-server-rest-2-tmg6xh47ja-uc.a.run.app/admin/v1/history?cursor=&limit=10&projectId=5679095853613056"
+fractalAdminHistory = "https://fractal-core-grpc-server-rest-2-tmg6xh47ja-uc.a.run.app/admin/v1/history?limit=10&projectId=5679095853613056"
 fractalAdminProjectHandleCinderTokenManage = "https://fractal-core-grpc-server-rest-2-tmg6xh47ja-uc.a.run.app/admin/v1/project/handle/cinder/token/manage/FvbByV9xZmiCEityxoypKfq3tgTe5ZmnQLVf5uGB95bT/history?cursor=&limit=10"
 
 #CoinGecko Public API https://www.coingecko.com/en/api/documentation
@@ -16,8 +16,14 @@ solscanAccountTransactions = "https://public-api.solscan.io/account/transactions
 
 fractalHistoryRaw = requests.get(fractalAdminHistory)
 fractalHistory = json.loads(fractalHistoryRaw.text)
-print(json.dumps(fractalHistory["transactions"][0], sort_keys=True, indent=4))
-print("There was a " + fractalHistory["transactions"][0]["type"] + " transaction at " + fractalHistory["transactions"][0]["time"] + " on Fractal.is")
-print("It was for " + fractalHistory["transactions"][0]["token"]["metaplex"]["name"] + " which has the rarity rank of " + str(fractalHistory["transactions"][0]["token"]["rarity"]["rank"]) + " which makes it a " + fractalHistory["transactions"][0]["token"]["rarity"]["type"] + " NFT")
-print("The transaction ID is " + fractalHistory["transactions"][0]["transactionId"])
-print(fractalHistory["transactions"][0]["toAddress"] + " paid " + fractalHistory["transactions"][0]["fromAddress"] + " " + str(fractalHistory["transactions"][0]["amount"]) + " SOL")
+#print(json.dumps(fractalHistory["transactions"], sort_keys=True, indent=4))
+i = 0
+#print(fractalHistory["next"]["token"])
+while i < int(fractalHistory["next"]["token"]):
+    print("<p>")
+    print("There was a " + fractalHistory["transactions"][i]["type"] + " transaction at " + fractalHistory["transactions"][i]["time"] + " on Fractal.is<br>")
+    print("It was for " + fractalHistory["transactions"][i]["token"]["metaplex"]["name"] + " which has the rarity rank of " + str(fractalHistory["transactions"][i]["token"]["rarity"]["rank"]) + " which makes it a " + fractalHistory["transactions"][i]["token"]["rarity"]["type"] + " NFT<br>")
+    print("The transaction ID is " + fractalHistory["transactions"][i]["transactionId"] + "<br>")
+    print(fractalHistory["transactions"][i]["toAddress"] + " paid " + fractalHistory["transactions"][i]["fromAddress"] + " " + str(fractalHistory["transactions"][i]["amount"]) + " SOL<br>")
+    print("</p>")
+    i = i + 1
